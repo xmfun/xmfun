8f0ecf5ef749327e30b0424bc17c25bc93bf89ad
ipmsteven <steven.lyl147@gmail.com>
2014-08-04 23:38:28 -0700

    yl - add parallel and progress bar support ready to refactor or die

--- Gemfile
+++ Gemfile
@@ -3,4 +3,5 @@ source "https://rubygems.org"
 gem 'clap'
 gem 'nokogiri'
 gem 'pry'
+gem 'parallel'
 gem 'ruby-mp3info'
--- xmfun.rb
+++ xmfun.rb
@@ -1,16 +1,14 @@
 #!/usr/bin/ruby
+
 require 'open-uri'
 require 'nokogiri'
 require 'clap'
 require 'mp3info'
+require 'pry'
+require 'parallel'
+require 'ruby-progressbar'
 require_relative "XMcode"
 
-
-#if ARGV[0].to_i == 0
-#url="http://www.xiami.com/song/playlist/id/"+ARGV[2].to_s+"/type/3"
-#else
-#url="http://www.xiami.com/song/playlist/id/"+ARGV[2].to_s+"/object_name/default/object_id/0"
-#end
 HELP = <<-HELP
 Usage: ruby xmfun.rb
   -c cid  -- download a music collect
@@ -20,58 +18,53 @@ Usage: ruby xmfun.rb
   -h      -- show help message
 
 Example:  Download Collect with ID 10181268 and save to current path)
-          ruby xmfun.rb -c 10181268 -d .
+          ./xmfun.rb -c 10181268 -d .
 HELP
 
 url = ''
 dst = '.'
 
+def download(url, dst)
+
+  if Dir.exist?(dst)
+    Dir.chdir(dst)
+  else
+    Dir.mkdir(dst)
+    Dir.chdir(dst)
+  end
+
+  f = Nokogiri::XML(open(url, "Client-IP" => "220.181.111.168"))
+  progress = ProgressBar.create(:title => "The Progress", :total => f.css("track").size )
+  Parallel.map(f.css("track"), :finish => lambda { |item, i, result| progress.increment }) do |track|
+    album     = track.css('album_name').text
+    artist    = track.css('artist').text
+    lyric     = track.css('lyric').text
+    title     = track.css('title').text
+    location  = track.css('location').text
+    album_pic = track.css('album_pic').text
+    mp3 = XMcode.decode(location)
+    uri = URI.parse(mp3)
+    uri.open(:content_length_proc => lambda { |content_length| content_length },
+             :progress_proc => lambda {|size| }) do |f|
+      File.open("#{title}_#{artist}.mp3", 'w') { |mp3| mp3.write(f.read) }
+    end
+    # add mp3 meta info
+    Mp3Info.open "#{title}_#{artist}.mp3" do |m|
+      m.tag.artist = artist
+      m.tag.title  = title
+      m.tag.album  = album
+
+      m.tag2.remove_pictures
+      m.tag2.add_picture(open(album_pic).read)
+    end
+  end
+end
+
 Clap.run ARGV,
   "-c" => lambda { |collect_id| url = "http://www.xiami.com/song/playlist/id/#{collect_id}/type/3" },
-"-s" => lambda { |song_id| url = "http://www.xiami.com/song/playlist/id/#{song_id}/object_name/default/object_id/0" },
-"-d" => lambda { |d| dst = d },
+  "-s" => lambda { |song_id| url = "http://www.xiami.com/song/playlist/id/#{song_id}/object_name/default/object_id/0" },
+  "-d" => lambda { |d| dst = d },
   "-v" => lambda { puts "0.0.1" },
   "-h" => lambda { puts HELP }
 
-if Dir.exist?(dst)
-  Dir.chdir(dst)
-else
-  Dir.mkdir(dst)
-  Dir.chdir(dst)
-end
-
-puts url
-
-f = Nokogiri::XML(open(url, "Client-IP" => "220.181.111.168"))
-
-f.css("track").each do |track|
-  album     = track.css('album_name').text
-  artist    = track.css('artist').text
-  lyric     = track.css('lyric').text
-  title     = track.css('title').text
-  location  = track.css('location').text
-  album_pic = track.css('album_pic').text
-
-  mp3 = XMcode.decode(location)
-
-  uri = URI.parse(mp3)
-  uri.open(:content_length_proc => lambda { |content_length| puts content_length },
-           :progress_proc => lambda {|size| }) do |f|
-    File.open("#{title}_#{artist}.mp3", 'w') { |mp3| mp3.write(f.read) }
-  end
-  
-  # add mp3 meta info
-  Mp3Info.open "#{title}_#{artist}.mp3" do |m|
-    m.tag.artist = artist
-    m.tag.title  = title
-    m.tag.album  = album
-
-    # Todo
-    #m.tag.lyric  = lyric 
-
-    m.tag2.remove_pictures
-    m.tag2.add_picture(open(album_pic).read)
-  end
-end
-
-
+download(url, dst)
