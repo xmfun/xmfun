4bb6054e7578d4689b991750aa5fa0bc26fd999c
ipmsteven <steven.lyl147@gmail.com>
2014-09-02 21:54:07 -0700

    yl - raise argument error if input is invalid and add test

--- lib/xmfun/util/decoder.rb
+++ lib/xmfun/util/decoder.rb
@@ -7,11 +7,17 @@ module Xmfun
   module Util
     class Decoder
       def self.decode(input)
-        input.match(/^(\d+)/)
-        row, text = $1.to_i, $'.chars.to_a
+        row, text = strtok(input)
 
         tmp = text.in_groups(row).transpose.join
         CGI::unescape(tmp).gsub('^', '0')
+      end
+
+      def self.strtok(s)
+        m = /^(\d+)/.match(s)
+        raise ArgumentError, "invalid input #{s}" unless m
+
+        [$1.to_i, $'.chars.to_a]
       end
     end
   end
