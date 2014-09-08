9c9f01091c398424517c9c17e912c24901415174
ipmsteven <steven.lyl147@gmail.com>
2014-09-07 18:22:34 -0700

    yl - escape special character from the mp3 file name

--- lib/xmfun/mp3/track.rb
+++ lib/xmfun/mp3/track.rb
@@ -1,12 +1,16 @@
 # -*- encoding : utf-8 -*-
 
 require 'ostruct'
+require 'cgi'
 
 module Xmfun
   module Mp3
     class Track
       def self.new(track_hash)
         track_hash["location"] = Xmfun::Util::Decoder.decode(track_hash["location"])
+        track_hash["title"]    = CGI.unescapeHTML(track_hash["title"])
+        track_hash["artist"]   = CGI.unescapeHTML(track_hash["artist"])
+
         track_hash["file_name"] = "#{track_hash["title"]}_#{track_hash["artist"]}.mp3"
         OpenStruct.new track_hash
       end
