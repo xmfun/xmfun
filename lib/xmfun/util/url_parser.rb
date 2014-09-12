0241e579982c67d018b5c26afd16b9fba6d65c59
ipmsteven <steven.lyl147@gmail.com>
2014-09-12 01:57:07 -0700

    yl - add patch album cover picture feature

--- lib/xmfun/util/url_parser.rb
+++ lib/xmfun/util/url_parser.rb
@@ -9,14 +9,19 @@ module Xmfun
           type, id = url_array[2], url_array[4]
 
           case type
-           when "album"   then "http://www.xiami.com/song/playlist/id/#{id}/type/1"
-           when "artist"  then "http://www.xiami.com/song/playlist/id/#{id}/type/2"
-           when "collect" then "http://www.xiami.com/song/playlist/id/#{id}/type/3"
-           when "song"    then "http://www.xiami.com/song/playlist/id/#{id}/object_name/default/object_id/0"
-           else ""
+            when "album" then
+              "http://www.xiami.com/song/playlist/id/#{id}/type/1"
+            when "artist" then
+              "http://www.xiami.com/song/playlist/id/#{id}/type/2"
+            when "collect" then
+              "http://www.xiami.com/song/playlist/id/#{id}/type/3"
+            when "song" then
+              "http://www.xiami.com/song/playlist/id/#{id}/object_name/default/object_id/0"
+            else
+              ""
           end
         else
-         false
+          false
         end
       end
 
